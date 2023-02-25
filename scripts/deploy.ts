60bc25e068a32ff64a6593f470c07a64467953df
scarfacedotcom <peteradaaku@gmail.com>
2023-02-25 12:22:28 +0100

    added other four contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,10 +5,59 @@ async function main() {
 
   const web3 = new Web3("https://eth-mainnet.g.alchemy.com/v2/IbkEKxpq0g2wcpd3XUqioRT4_-nsa0dv");
 
-  const Aave = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
-  const AaveValue = await web3.eth.getStorageAt(Aave, 3);
-  console.log(`Aave value is ${AaveValue}`);
-  
+  // const Aave = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
+  // const AaveValue = await web3.eth.getStorageAt(Aave, 3);
+  // console.log(`Aave value is ${AaveValue}`);
+
+  console.log(`------------------------READING FROM AAVES CONTRACTS STORAGE--------------------------------`)
+
+  let AavecontractAddress = '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9';
+  let index = 0;
+for (index = 0; index < 10; index++){
+ console.log(`[${index}]` + 
+   await web3.eth.getStorageAt(AavecontractAddress, index))
+}
+
+
+console.log(`------------------------READING FROM SUSHI CONTRACTS STORAGE--------------------------------`)
+
+let SushicontractAddress = '0x6B3595068778DD592e39A122f4f5a5cF09C90fE2';
+for (index = 0; index < 10; index++){
+console.log(`[${index}]` + 
+ await web3.eth.getStorageAt(SushicontractAddress, index))
+}
+
+console.log(`------------------------READING FROM DECENTRALAND MANA CONTRACTS STORAGE--------------------------------`)
+
+let ManacontractAddress = '0x0F5D2fB29fb7d3CFeE444a200298f468908cC942';
+for (index = 0; index < 10; index++){
+console.log(`[${index}]` + 
+ await web3.eth.getStorageAt(ManacontractAddress, index))
+}
+
+console.log(`------------------------READING FROM FANTOM TOKEN CONTRACTS STORAGE--------------------------------`)
+
+let FTMcontractAddress = '0x4E15361FD6b4BB609Fa63C81A2be19d873717870';
+for (index = 0; index < 11; index++){
+console.log(`[${index}]` + 
+ await web3.eth.getStorageAt(FTMcontractAddress, index))
+}
+
+console.log(`------------------------READING FROM SAND CONTRACTS STORAGE--------------------------------`)
+
+let SandcontractAddress = '0x3845badAde8e6dFF049820680d1F14bD3903a5d0';
+for (index = 0; index < 10; index++){
+console.log(`[${index}]` + 
+ await web3.eth.getStorageAt(SandcontractAddress, index))
+}
+
+console.log(`------------------------READING FROM CHILIZ CONTRACTS STORAGE--------------------------------`)
+
+let ChzcontractAddress = '0x3506424F91fD33084466F402d5D97f05F8e3b4AF';
+for (index = 0; index < 10; index++){
+console.log(`[${index}]` + 
+ await web3.eth.getStorageAt(ChzcontractAddress, index))
+}
 }
 
 // We recommend this pattern to be able to use async/await everywhere
